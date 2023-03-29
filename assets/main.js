c201276980e3494656582c7705749820fad8b3cd
SalvatoreBrancato <salvatorebrancato90@gmail.com>
2023-03-30 01:41:17 +0200

    Struttura_definitiva2

--- assets/main.js
+++ assets/main.js
@@ -3,11 +3,11 @@ const lista = ["Pane", "Latte","Zucchero","Acqua"];
 const ul = document.querySelector("ul");
 let i = 0;
 while(i < lista.length){
-     
+    ul.innerHTML +=`<li>${lista[i]}</li>`;
     i++;
 
-    ul.innerHTML +=`<li>${lista[i]}</li>`;
+    
 }
 
 
-    
+
